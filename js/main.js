fee5accaa98c5078d769289b5f2f7253209e116a
Nico MP <nicopinguel@gmail.com>
2017-02-28 21:05:56 -0500

    fixed js bugs and updated stylesheets

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 //First see if it exists, if not create an empty object
 var OPW = OPW || {};
-OPW.utilities ={
+OPW.utilities = { 
 	listen: function(){
 		//attach click event to .js-menu
 		$('.js-menu').on('click', function(){
 			//do things
 			$('.nav-main').slideToggle();
-	});
+		});
 
-	$(window).on('resize', function(){
-		var w = $(window).width();
-		//write an if stattement, if width > 480, do something
-		if (w > 480 && $('.nav-main').is('[style]')){
+		$(window).on('resize', function(){
+			var w = $(window).width();
+			//write an if stattement, if width > 480, do something
+			if (w > 480 && $('.nav-main').is('[style]')){
 				console.log('removed style');
 				$('.nav-main').removeAttr('style');
 			}	
 		}); //closes window.resize event
+
 		// attach click event to main-nav item
 		$('.nav-main a').on('click', function(event){
 			console.log(event);
@@ -31,8 +32,8 @@ OPW.utilities ={
 			//store $(this).attr('href') in a variable
 			var pageToScrollTo = $(this).attr('href');
 			//use that variable in our $.scrollTo() function
-			$(window).scrollTo(pageToScrollTo, 800);
-		})
+			$(window).scrollTo(pageToScrollTo, 800 , {offset:-96});
+		});
 
 	}
 }
